2cff5cb53e38137c314bf55056da7d009eb0a29a
Samandar3464 <107240826+Samandar3464@users.noreply.github.com>
2023-01-16 16:33:11 +0500

    basket

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,4 @@
 package org.example;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-
 public class Main {
     public static void main(String[] args) {
     }
